8946a47cf4348ffb8c553277a48027bee1bf9b90
Gyorgy Nadudvari <reedcourty@gmail.com>
2012-11-24 16:37:29 +0100

    Sikerült belőni, hogy fejlesztés közben is működjenek a media, static kiszolgálások.

--- badge_out/badge_out/settings.py
+++ badge_out/badge_out/settings.py
@@ -53,7 +53,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = PROJECT_PATH + '/media'
+MEDIA_ROOT = PROJECT_PATH + '/../media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_PATH + '/../static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
