6618cc31192dc7e6299df90f1a88950118a286eb
Gyorgy Nadudvari <reedcourty@gmail.com>
2012-11-29 10:54:21 +0100

    Most már tudunk badge-et törölni. (closed #17)

--- badge_out/badge/views.py
+++ badge_out/badge/views.py
@@ -553,3 +553,51 @@ def manage_badge_delete(request, id):
         return render_to_response('manage-badge.html',
                                   {'content' : content},
                                   context_instance = RequestContext(request))
+
+@login_required    
+def manage_badge_edit(request, id):
+    
+    if (not is_oktato(request)):
+        return HttpResponseRedirect('/start')
+    else:
+        badge = Badge.objects.get(pk=id)
+        
+        content = {
+            'operation': 'edit',
+            'badge' : badge,
+            'badge_form' : None,
+            'error' : None,
+            'request': request, 
+        }
+        
+        if (request.method == 'POST'):
+            
+            nev = request.POST['nev']
+            leiras = request.POST['leiras']
+            
+            kep = request.FILES.get('kep')
+            
+            content['POST'] = request.POST
+            content['FILES'] = request.FILES
+            
+            content['badge_form'] = {
+                'nev' : nev,
+                'leiras' : leiras,
+                'kep' : kep
+            }
+            
+            if (nev == "") or (leiras == ""):
+                content['error'] = u"Nem adtál meg minden adatot! :("
+            else:
+
+                badge.nev = nev
+                badge.leiras = leiras
+                if (kep != None):
+                    badge.kep.save(kep.name, kep)
+                                
+                badge.save()
+                return HttpResponseRedirect('/manage/badge/')
+            
+        return render_to_response('manage-badge.html',
+                                  {'content' : content},
+                                  context_instance = RequestContext(request))
